2b5a335838f9afee36b1db7465cebfbcc2053cd1
rand52 <124181900+rand52@users.noreply.github.com>
2024-12-12 18:55:35 +0100

    Added raw data files

---
+++ 2D-Datapoint.py
@@ -0,0 +1,6 @@
+class twoD_DP:
+    def __init__(self):
+        """Constructor for the 2D datapoint class. Takes in all measured data for a certain AOA["""
+        self.airfoil_p_taps = []
+        self.rake_static_p_taps = []
+        self.rake_total_p_taps = []
