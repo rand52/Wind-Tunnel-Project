f908828f92d7c2b82fda9006a145878cb5a4702e
rand52 <124181900+rand52@users.noreply.github.com>
2024-12-13 18:58:40 +0100

    updated to use pandas instead as it's more efficient and numpy arrays are nice

--- twoD_Data_Reader.py
+++ twoD_Data_Reader.py
@@ -1,36 +1,39 @@
+import pandas as pd
+import scipy as sc
+
 import twoD_Datapoint as dp
 
-# datafile
+# txt datafile with test results
 datafile = "raw_2D.txt"
-comment_indicator = "#" # line indicator NOT read
-header = 2 # header NOT read size
+comment_indicator = "#"  # line indicator NOT read
+header = 2  # header NOT read size
+
+# Read the file into a Pandas DataFrame
+df = pd.read_csv(
+    datafile,
+    sep=r"\s+", # delimiter = whitespaces
+    comment=comment_indicator,  # line indicator NOT read
+    skiprows=header,  # don't read header lines
+    header=None  # use int headers for columns and rows
+)
 
 # list with the datapoints
 datapoints = []
 
-with open(datafile, 'r') as file:
-    l=0
-    for line in file:
-        l+=1
-        if l<=header: continue # skip header
-        line = line.strip()
-        if not line.startswith(comment_indicator): # skip commented lines
-            datPt = dp.twoD_DP() # create new datapoint
-            e=0
-            for entry in line.split():
-                e+=1
-                if e>2: entry_val=float(entry)
-                # save the datapoint entries in the correct fields
-                match e:
-                    case 3 : datPt.aoa=entry_val
-                    case 4 : datPt.del_pb=entry_val
-                    case 5 : datPt.p_atm=entry_val*100 # it's given in kPa
-                    case 6 : datPt.temp_C=entry_val
-                    case 8 : datPt.rho=entry_val
-                    case 105: datPt.p_stat=entry_val # P097
-                    case x if (x >= 9 and x <= 33): datPt.airfoil_top_p_taps.append(entry_val) # P001-P025
-                    case x if (x >= 34 and x <= 57): datPt.airfoil_bottom_p_taps.append(entry_val) # P026-P049
-                    case x if (x >= 58 and x <= 104): datPt.rake_total_p_taps.append(entry_val) # P050-P096
-                    case x if (x >= 106 and x <= 117): datPt.rake_total_p_taps.append(entry_val) # P098-P109
-            datapoints.append(datPt)
+for row in df.values:
+    datPt = dp.twoD_DP()  # create new datapoint
+    # save data to the datapoint
+    datPt.aoa = row[2]
+    datPt.del_pb = row[3]
+    datPt.p_tot_inf = row[4] * 100  # it's given in kPa
+    datPt.temp_C = row[5]
+    datPt.rho_st_ch = row[7]
+    datPt.airfoil_top_p_taps = row[8:33:]  # P001-P025
+    datPt.airfoil_bottom_p_taps = row[33:57:]   # P026-P049
+    datPt.rake_total_p_taps = row[57:104:]   # P050-P096
+    datPt.rake_static_p_taps = row[105:117:]   # P098-P109
+    datapoints.append(datPt)
 
+
+#datapoints[0].plot_pressures()
+print(datapoints[0].rake_static_p_taps)
