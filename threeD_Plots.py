6217ea1f4f78911a4ebcb0f6497a3ff9b61dd85d
ivani <ivanivanov0403@gmail.com>
2024-12-14 17:47:43 +0100

    kur

--- threeD_Plots.py
+++ threeD_Plots.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,14 +15,17 @@ data_sorted = data.sort_values(by="Alpha")
 # Extract relevant columns
 alpha = data_sorted["Alpha"]  # Alpha (degrees)
 fy = data_sorted["Fy"]        # Fy (N)
+CL = fy / (0.5 * 1.19 * 20**2 * 0.064)
 
+Slope3D = (CL[8]-CL[0]) * (180 / math.pi) / (14)
 
+print(Slope3D)
 
 # Plot Fy vs Alpha
 plt.figure(figsize=(8, 6))
-plt.plot(alpha, fy, marker='s', linestyle='-', color='g', label='Experiment')
+plt.plot(alpha, CL, marker='s', markersize=3, linestyle='-', color='g', label='Experiment')
 x_major_locator = MultipleLocator(5)  # Major ticks every 5 units on x-axis
-y_major_locator = MultipleLocator(5)  # Major ticks every 5 units on y-axis
+y_major_locator = MultipleLocator(0.1)  # Major ticks every 5 units on y-axis
 
 ax = plt.gca()  # Get current axes
 ax.xaxis.set_major_locator(x_major_locator)
