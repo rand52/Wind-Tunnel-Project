37f056c443490b5d23e86c47b3922532b6229374
rand52 <124181900+rand52@users.noreply.github.com>
2024-12-16 13:01:10 +0100

    plotter update

--- twoD_plotter.py
+++ twoD_plotter.py
@@ -1,13 +1,14 @@
 from twoD_Data_Reader import datapoints
 import twoD_Datapoint as dp
 
-for i in datapoints:
+#for i in datapoints:
 #     i.plot_pressures()
-    i.plot_Cp()
+#    i.plot_Cp()
 #     i.plot_Velocity_Deficit()
 #     i.plot_Velocity_Deficit(mode="fraction",neg_noise_reduction=False)
 
 #dp.plot_CL_AOA_curve(datapoints[0:39:])
 #dp.plot_drag_polar(datapoints[0:39:])
 #dp.plot_Xcp_AOA_curve(datapoints[0:39:])
-#dp.plot_Cm_AOA_curve(datapoints[0:39:],mode="le")
+dp.plot_Cm_AOA_curve(datapoints[0:39:],mode="quarter")
+dp.plot_Cm_AOA_curve(datapoints[0:39:],mode="le")
